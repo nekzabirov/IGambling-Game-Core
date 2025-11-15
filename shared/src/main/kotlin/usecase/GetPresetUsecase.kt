fad6af14b5e626fcb4ab4d0963ba8d27b17428c4
nekzabirov <n.zabirov@1638.tech>
2025-11-15 21:32:51 +0200

    Add validation for free spin enablement in `GetPresetUsecase`
    - Introduced `BadRequestException` for handling cases where free spins are not enabled.
    - Updated `getPresetResult` logic to include validation checks.

--- shared/src/main/kotlin/usecase/GetPresetUsecase.kt
+++ shared/src/main/kotlin/usecase/GetPresetUsecase.kt
@@ -3,12 +3,17 @@ package usecase
 import domain.aggregator.adapter.PresetParam
 import domain.game.service.GameService
 import infrastructure.aggregator.AggregatorFabric
+import io.ktor.server.plugins.BadRequestException
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 class GetPresetUsecase {
     suspend operator fun invoke(gameIdentity: String): Result<Response> = newSuspendedTransaction {
         val game = GameService.findByIdentity(gameIdentity)
             .getOrElse { return@newSuspendedTransaction Result.failure(it) }
+
+        if (!game.freeSpinEnable) {
+            return@newSuspendedTransaction Result.failure(BadRequestException("Free spin is not enabled"))
+        }
 
         val adapter = AggregatorFabric.createAdapter(game.aggregator.config, game.aggregator.aggregator)
 
